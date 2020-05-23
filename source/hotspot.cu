5c4bded758e9b88f122c4927cabba6e976c04806
jorgesimoes <jorgesimoes+gitlab@tecnico.ulisboa.pt>
2020-05-23 23:53:40 +0100

    testing

--- source/hotspot.cu
+++ source/hotspot.cu
@@ -160,11 +160,11 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
         exit(EXIT_FAILURE);
     }
     //printf("result-dev[17597] - %lf\n", result_dev[17597]);
-    //err = cudaMemcpy(result, result_dev, (size_t)(sizeof(FLOAT)*col*row), cudaMemcpyDeviceToHost);                                                            
-    err = cudaMemcpy(&DEBUG_INT, size_dev, (size_t)(sizeof(FLOAT)), cudaMemcpyDeviceToHost);                                                            
-    printf("size - %d\n", DEBUG_INT);
+    err = cudaMemcpy(result, result_dev, (size_t)(sizeof(FLOAT)*col*row), cudaMemcpyDeviceToHost);                                                            
+    //err = cudaMemcpy(&DEBUG_INT, size_dev, (size_t)(sizeof(FLOAT)), cudaMemcpyDeviceToHost);                                                            
+    //printf("size - %d\n", DEBUG_INT);
 
-    err = cudaMemcpy(DEBBUG_HOST, DEBUG, (size_t)(sizeof(FLOAT)*col*row), cudaMemcpyDeviceToHost);                                                            
+    //err = cudaMemcpy(DEBBUG_HOST, DEBUG, (size_t)(sizeof(FLOAT)*col*row), cudaMemcpyDeviceToHost);                                                            
     //for (int i = 0; i < 1024*1024; i++)
     //    printf("DEBUG[%d] - %lf   temp[%d] - %lf\n",i, DEBBUG_HOST[i], i, temp[i]);
     
