974122c2fb2f75a98abf05c54897d02176d01928
jorgesimoes <jorgesimoes+gitlab@tecnico.ulisboa.pt>
2020-05-23 23:37:27 +0100

    testing

--- source/hotspot.cu
+++ source/hotspot.cu
@@ -59,19 +59,19 @@ __global__ void kernel (FLOAT *Ry_1_dev, FLOAT *Rx_1_dev, FLOAT *Rz_1_dev,
             && column > BLOCK_SIZE_C_dev && column < size-BLOCK_SIZE_C_dev) */
     
     int size = *size_dev;
-    *size_dev = 1023;
+    *size_dev = row;
     //result_dev[size*size] = 1; 
+    /*
     if (row > 15 && row < size - 1 &&  column < size - 1 ) {
         DEBUG[row*size+column] = temp_dev[row*size+column];
-        result_dev[row*size+column] = 1.f; /*
         result_dev[row*size+column] =temp_dev[row*size+column]+ 
              ( (*Cap_1_dev) * (power_dev[row*size+column] + 
             (temp_dev[(row+1)*size+column] + temp_dev[(row-1)*size+column] - 2.f*temp_dev[row*size+column]) * (*Ry_1_dev) + 
             (temp_dev[row*size+column+1] + temp_dev[row*size+column-1] - 2.f*temp_dev[row*size+column]) * (*Rx_1_dev) + 
             (amb_temp_dev - temp_dev[row*size+column]) * (*Rz_1_dev)));
-            */
+            
     }
-    
+    */
 
     /*
     for ( r = r_start; r < r_start + BLOCK_SIZE_R; ++r ) {
