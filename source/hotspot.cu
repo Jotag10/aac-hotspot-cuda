2512d596e34cb1af7d0d00ad28357a274411f48c
jorgesimoes <jorgesimoes+gitlab@tecnico.ulisboa.pt>
2020-05-26 18:55:04 +0100

    testing

--- source/hotspot.cu
+++ source/hotspot.cu
@@ -64,7 +64,7 @@ __global__ void kernel (FLOAT *Ry_1_dev, FLOAT *Rx_1_dev, FLOAT *Rz_1_dev,
     int size = *size_dev;
     //result_dev[size*size] = 1; 
     //if (column < size*size - 1  && column > size+1) {
-    if (row < size - 1  && row > 1) {
+    if (row < 1023  && row > 1) {
         //*size_dev = 1023;
         //DEBUG[row*size+column] = temp_dev[row*size+column];
         /*
